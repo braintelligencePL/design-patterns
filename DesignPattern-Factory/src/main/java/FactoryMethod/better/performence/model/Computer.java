6d89e35ec8c62282c87cc7bd10ff900988d1de98
Lukasz Sroczynski <lukasz.sroczynski@allegro.pl>
2018-02-08 15:39:20 +0100

    cleanup

--- DesignPattern-Factory/src/main/java/FactoryMethod/better/performence/model/Computer.java
+++
@@ -1,8 +0,0 @@
-package FactoryMethod.better.performence.model;
-
-public interface Computer {
-    String getSSD();
-    String getRAM();
-    String getCPU();
-}
-
--- DesignPattern-Factory/src/main/java/FactoryMethod/less/performence/model/Computer.java
+++
@@ -1,8 +0,0 @@
-package FactoryMethod.less.performence.model;
-
-public interface Computer {
-    String getSSD();
-    String getRAM();
-    String getCPU();
-}
-
